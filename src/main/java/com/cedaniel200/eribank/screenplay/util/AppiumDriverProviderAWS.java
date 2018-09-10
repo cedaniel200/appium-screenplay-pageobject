51f46d4d4a43ba1980ab1d8b63943f0b3fde7595
Danny Andres Barrientos <dannybarrientos12@gmail.com>
2018-09-09 20:34:49 -0500

    Configuration AWS Capabilities PlatformName Android

--- src/main/java/com/cedaniel200/eribank/screenplay/util/AppiumDriverProviderAWS.java
+++ src/main/java/com/cedaniel200/eribank/screenplay/util/AppiumDriverProviderAWS.java
@@ -1,29 +1,28 @@
 package com.cedaniel200.eribank.screenplay.util;
 
-import io.appium.java_client.AppiumDriver;
+
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class AppiumDriverProviderAWS {
 
     public static WebDriver getDriver() {
         AndroidDriver<MobileElement> driver;
 
+
         DesiredCapabilities capabilities= new DesiredCapabilities();
-        capabilities.setCapability("BROWSER_NAME", "Android"); // En la pagina estaba Android
-        capabilities.setCapability("VERSION", "8.1.0");
-        capabilities.setCapability("deviceName", "Emulator");
-        capabilities.setCapability("platformName", "Android"); // En la pagina estaba Adroid
-       // capabilities.setCapability("appPackage", "com.experitest.ExperiBank");
-        //capabilities.setCapability("appActivity", "com.experitest.ExperiBank.LoginActivity");
+        capabilities.setCapability("platformName", "ANDROID");
+        capabilities.setCapability("deviceName", "asdf");
+        capabilities.setCapability("platformVersion", "7.0");
+        capabilities.setCapability("appPackage", "com.experitest.ExperiBank");
+        capabilities.setCapability("appActivity", "com.experitest.ExperiBank.LoginActivity");
+        capabilities.setCapability("noReset", true);
+
 
         URL url = null;
         try {
--- src/test/java/com/cedaniel200/eribank/screenplay/cloud/AuthenticationCloud.java
+++ src/test/java/com/cedaniel200/eribank/screenplay/cloud/AuthenticationCloud.java
@@ -2,27 +2,26 @@ package com.cedaniel200.eribank.screenplay.cloud;
 
 import com.cedaniel200.eribank.screenplay.questions.TheHomePage;
 import com.cedaniel200.eribank.screenplay.tasks.Authenticate;
-import net.serenitybdd.junit.runners.SerenityRunner;
+import com.cedaniel200.eribank.screenplay.util.AppiumDriverProviderAWS;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
-import net.thucydides.core.annotations.Managed;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import static com.cedaniel200.eribank.screenplay.model.builder.UserBuilder.user;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 
-@RunWith(SerenityRunner.class)
+
 public class AuthenticationCloud {
 
-    @Managed(driver = "Appium")
+
     private WebDriver driver;
     private Actor cesar = Actor.named("Cesar");
 
     @Before
     public void prepareStage() {
+        driver = AppiumDriverProviderAWS.getDriver();
         cesar.can(BrowseTheWeb.with(driver));
     }
 
